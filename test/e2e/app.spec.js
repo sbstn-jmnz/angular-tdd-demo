e3fafbfdf0c8877271f8f260dd9935f2a9282c96
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-12-05 20:08:03 -0300

    more end to end test

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,5 +1,12 @@
 describe("End to end address test", function(){
-  it('has contacts', function() {
+  it('has contacts', function(done) {
     browser.get('http://localhost:8080');
+    element.all(by.repeater('contact in contacts'))
+    .then(function(contacts){
+      var first = contacts[0];
+      var text = first.getText();
+      expect(text).toEqual('Robert R');
+      done();
+    });
   });
 });
