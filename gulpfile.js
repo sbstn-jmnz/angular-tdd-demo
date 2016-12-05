331662034dafae75e924c445469c0e6c440e2290
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-12-05 19:15:56 -0300

    code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -23,12 +23,26 @@ gulp.task('serve', ['server'], function serve() {
   .on('change', browserSync.reload);
 });
 
-gulp.task('test-browser', function() {
+gulp.task('test-browser', function(done) {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true,
-    reporters: ['mocha']
+    reporters: ['mocha', 'coverage']
+  }, function() {
+    done();
   });
+});
+
+gulp.task('serve-coverage',['test-browser'], function serve() {
+  browserSync.init({
+    notify: false,
+    port: 7777,
+    server: {
+      baseDir: ['test/coverage'],
+    },
+  });
+  gulp.watch(['app/**/*.*'])
+  .on('change', browserSync.reload);
 });
 
 gulp.task('serve-test', function serve() {
--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,24 @@
 module.exports = function(config){
   config.set({
-    plugins:['karma-mocha','karma-phantomjs-launcher','karma-mocha-reporter'],
+    //plugins:['karma-mocha','karma-phantomjs-launcher','karma-mocha-reporter'],
     browsers: ['PhantomJS'],
     frameworks: ['mocha'],
+    preprocessors: {
+      'app/**/*.js': ['coverage']
+    },
+    coverageReporter: {
+      includeAllSources: true,
+      reporters: [{
+        type: 'html',
+        dir: 'test/coverage',
+        subdir: '.'
+      },{
+        type: 'text'
+      }]
+    },
     files: [
       'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/chai/chai.js',
 
       'app/**/*.js',
