7449c2ad9e07fda7b6dc640ad2fe65dac0220078
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2016-12-05 16:57:54 -0300

    Testing Services with inject and module

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,15 @@
-describe("The Address Book App", function() {
-  it("should work", function(){
-    chai.assert.isArray([]);
+var assert = chai.assert;
+var expect = chai.expect;
+
+describe("The Address Book App", function(){
+  describe("The contact service", function(){
+    it('have a property contacts, an array', function(){
+      module('AddressBook');
+      inject(function($injector) {
+        contactService = $injector.get('contactService');
+      });
+
+      expect(contactService.contacts).to.be.an('array');
+    });
   });
 });
